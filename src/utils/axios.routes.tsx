8e5b368b1e0a5f4d8f0abb00feae0e7b4c5a36b9
Valderi Douglas <valderi.douglas.1000@gmail.com>
2023-05-14 19:12:25 -0300

    corrigindo update de parcelas

--- src/utils/axios.routes.tsx
+++ src/utils/axios.routes.tsx
@@ -414,7 +414,7 @@ export const updateCliente = async (cpf: any, value: any) => {
 
 export const updateParcela = async(id:any , valorPago:any) => {
   try {
-    const response = await api.put(`/atualizar/parcela/${id}`, {
+    const response = await api.post(`/atualizar/parcela/${id}`, {
       valor_pago: valorPago,
       status: true,
     }, {
