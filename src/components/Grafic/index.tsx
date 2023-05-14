7a2140d0df76f91aefe9da3e0c50c052fd1a16d4
Wallace Felipe <wallacefelipe21@outlook.com>
2023-05-14 01:27:15 -0300

    Novo Sistema de Graficos e estatisticas

--- src/components/Grafic/index.tsx
+++ src/components/Grafic/index.tsx
@@ -1,58 +1,49 @@
-import React, { PureComponent } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: "Semana 01/05 até 07/05",
+    Adimplente: 20,
+    Inadimplente: 5,
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: "Semana 08/05 até 14/05",
+    Adimplente: 22,
+    Inadimplente: 5,
   },
   {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: "Semana 15/05 até 21/05",
+    Adimplente: 20,
+    Inadimplente: 5,
   },
   {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: "Semana 22/05 até 28/05",
+    Adimplente: 29,
+    Inadimplente: 5,
   },
   {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: " Semana 29/05 até 31/05",
+    Adimplente: 20,
+    Inadimplente: 9,
   },
 ];
 
 export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/stacked-area-chart-ix341';
+  static demoUrl = "https://codesandbox.io/s/stacked-area-chart-ix341";
 
   render() {
     return (
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
+      <ResponsiveContainer width="70%" height="70%">
+        <BarChart
           width={500}
           height={400}
           data={data}
@@ -65,12 +56,14 @@ export default class Example extends PureComponent {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
-          <YAxis />
+          <YAxis
+            ticks={[0, 5, 10, 15, 20, 25, 30]}
+            tickCount={7}
+          />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-          <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
-        </AreaChart>
+          <Bar dataKey="Adimplente" barSize={20} fill="#82ca9d" />
+          <Bar dataKey="Inadimplente" barSize={20} fill="#ffc658" />
+        </BarChart>
       </ResponsiveContainer>
     );
   }
--- src/pages/Analytics/index.tsx
+++ src/pages/Analytics/index.tsx
@@ -1,109 +1,66 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Graphic from '../../components/Grafic'
-import Navbar from '../../components/Navbar'
-import './style.css'
-import { dadosUsuario } from '../../utils/axios.routes';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
+import React, { useContext, useEffect, useState } from "react";
+import Graphic from "../../components/Grafic";
+import Navbar from "../../components/Navbar";
+import "./style.css";
+import { dadosUsuario } from "../../utils/axios.routes";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Analytics = () => {
   const navigate = useNavigate();
-  const { isLogged, funcionario } = useContext(AuthContext)
+  const { isLogged, funcionario } = useContext(AuthContext);
 
   const [data, setData] = useState<any>();
 
   useEffect(() => {
-    if(!isLogged){
-      navigate('/')
+    if (!isLogged) {
+      navigate("/");
     }
-      const fetchData = async () => {
-          try {
-              const response = await dadosUsuario(2);
-              const data = await response?.data
-              setData(data);
-          } catch (error) {
-              console.error(error);
-          }
-      };
+    const fetchData = async () => {
+      try {
+        const response = await dadosUsuario(2);
+        const data = await response?.data;
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-      fetchData();
+    fetchData();
   }, []);
-  if(isLogged && (funcionario.cargo == 'Administrador' || funcionario.cargo == 'Financeiro')){
+  if (
+    isLogged &&
+    (funcionario.cargo == "Administrador" || funcionario.cargo == "Financeiro")
+  ) {
     return (
       <>
-      <Navbar/>
-      <div className='main'>
-        <div className="top-box">
-          <h3>Clientes Adimpletes e Inadimplentes</h3>
-          <div className="select-box">
-            <div className="select-input">
-              <h3>Data de Inicio</h3>
-              <input
-                required
-                type="date"
-              />
-            </div>
-            <div className="select-input">
-              <h3>Data de Fim</h3>
-              <input
-                required
-                type="date"
-              />
-            </div>
-            <div className='select-input'>
-              <h3>Status</h3>
-            <select name="choice">
-              <option value="first">---- Selecione ----</option>
-              <option value="first">Data de Crédito</option>
-              <option value="second">Data de Pagamento</option>
-              <option value="third">Data de Vencimento</option>
-            </select>
-            </div>
+        <Navbar />
+        <div className="container-stats">
+          <h2>Cadastro de Clientes</h2>
+          <div className="button-select-graphics">
+            <button className="button-graphics">Situação dos Clientes</button>
+            <button className="button-graphics">Situação dos Pagamento</button>
+            <button className="button-graphics">Valores</button>
           </div>
         </div>
-        <Graphic />
-      </div>
-    </>
-  );
-} else if(isLogged && funcionario.cargo == 'Comercial'){
-  return(
-    <>
-      <Navbar/>
-      <div className='main'>
-        <div className="top-box">
-          <h3>Clientes Atrasados com o pagamento</h3>
-          <div className="select-box">
-            <div className="select-input">
-              <h3>Data de Inicio</h3>
-              <input
-                required
-                type="date"
-              />
-            </div>
-            <div className="select-input">
-              <h3>Data de Fim</h3>
-              <input
-                required
-                type="date"
-              />
-            </div>
-            <div className='select-input'>
-              <h3>Status</h3>
-            <select name="choice">
-              <option value="third">Data de Vencimento</option>
-            </select>
-            </div>
+      </>
+    );
+  } else if (isLogged && funcionario.cargo == "Comercial") {
+    return (
+      <>
+        <Navbar />
+        <div className="container-stats">
+          <h2>Cadastro de Clientes</h2>
+          <div className="button-select-graphics">
+            <button className="button-graphics">Situação dos Clientes</button>
+            <button className="button-graphics">Situação dos Pagamento</button>
+            <button className="button-graphics">Valores</button>
           </div>
         </div>
-        <Graphic />
-      </div>
-    </>
-  )
-} else{
-  return(
-    <></>
-  )
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
-
-}
 export default Analytics;
